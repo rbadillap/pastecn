ec9f5a63d0f2c348d0af9a9c4b8863211a128fa8
Ronny Badilla <info@ronnybadilla.com>
2026-01-19 06:47:19 -0600

    feat: demo page

---
+++ app/welcome/page.tsx
@@ -0,0 +1,9 @@
+export default function Welcome() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl md:text-8xl font-semibold font-brand text-primary/80 text-center">
+        Welcome to pastecn!
+      </h1>
+    </main>
+  )
+}
