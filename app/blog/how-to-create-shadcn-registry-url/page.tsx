f44ada3283f0857c38d7484846b0bfbfbae1068b
Ronny Badilla <rbadillap@gmail.com>
2026-01-27 14:19:49 -0600

    fix: github (#32)

--- app/blog/how-to-create-shadcn-registry-url/page.tsx
+++ app/blog/how-to-create-shadcn-registry-url/page.tsx
@@ -193,7 +193,7 @@ export default function BlogPostRegistryUrl() {
             </Link>
           </Button>
           <Button asChild size="lg" variant="outline" className="w-full sm:w-auto">
-            <a href="https://github.com/ronny/pastecn" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/rbadillap/pastecn" target="_blank" rel="noopener noreferrer">
               <Icons.github className="mr-2 h-4 w-4" />
               Star us on GitHub
             </a>
